1d19defaaf6447e284b6ae2c1b92798c42fd9e51
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-04 01:46:14 -0500

    Added gameplay mechanics.
    Current Bugs:
    New game button does not work
    Round Display moves after buttons are shown

--- scripts.js
+++ scripts.js
@@ -16,8 +16,25 @@ function startScreenElements() {
         playerTwoNameLabel, playerTwoNameInput, playGameButton}
 }
 
+function gameScreenElements() {
+    const gameBoard = document.querySelector("#gameBoard")
+    const playerOneScore = document.querySelector("#playerOneDisplay")
+    const playerTwoScore = document.querySelector("#playerTwoDisplay")
+    const playBoard = document.querySelector("#playBoard")
+    const cells = document.querySelectorAll("#playBoard .cell");
+    const roundDisplay = document.querySelector("#roundDisplay")
+    const roundDisplayText = document.querySelector("#roundDisplay .text")
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].id = `cell-${i + 1}`; 
+      }
+
+    return {gameBoard, playerOneScore, playerTwoScore, playBoard, cells, roundDisplay, roundDisplayText}
+}
+
 function screenManager() {
     startScreenUI = startScreenElements()
+    startScreenUI.playGameButton.addEventListener("click", startGame)
 
     function startGame() {
         if (startScreenUI.playerOneNameInput.value.length === 0) {
@@ -42,47 +59,36 @@ function screenManager() {
         
         playerOne = new player(1, startScreenUI.playerOneNameInput.value, 0)
         playerTwo = new player(2, startScreenUI.playerTwoNameInput.value, 0)
-        
-        startScreenUI.nameForm.remove()
 
         let gameBoard = document.createElement("div")
         gameBoard.id = "gameBoard"
         gameBoard.innerHTML =
         `
         <div id="scoreDisplay">
-            <div id=playerOneDisplay>Player 1: 0
+            <div id=playerOneDisplay class="display scoreDisplay">${playerOneNameInput.value}: 0
             </div>
-            <div id=playerTwoDisplay>Player 2: 0
+            <div id=playerTwoDisplay class="display scoreDisplay">${playerTwoNameInput.value}: 0
             </div>
         </div>
         <div id="playBoard">
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
-            <div class=cell type=button>
-            </div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
+            <div class=cell type=button></div>
         </div>
-        <div id=roundDisplay>
+        <div id=roundDisplay class="display">
             <div class="text">Round: 1</div>
         </div>
         `
+        startScreenUI.nameForm.remove()
         startScreenUI.content.appendChild(gameBoard)
+        gameManager(playerOne, playerTwo)
     }
-
-    startScreenUI.playGameButton.addEventListener("click", startGame)
 }
 
 function player(playerNumber, name, points) {
@@ -90,13 +96,138 @@ function player(playerNumber, name, points) {
     this.name = name
     this.points = points
     this.win = function() {
-        points++
+        this.points += 1
     }
 }
 
-function gameManager() {
+function gameManager(playerOne, playerTwo) {
+    var gameScreenUI = gameScreenElements()
+    gameScreenUI.roundDisplay.style.marginBottom = "10vh"
+    let roundNumber = 0
+    let currentPlayerTurn = Math.random() <= 0.5 ? 1 : 2
+    playerDisplayFocus(currentPlayerTurn)
+    let gameCompleted = false;
 
+    for (const cell of gameScreenUI.cells) {
+      cell.addEventListener("click", handleCellClick);
+    }
+
+    function handleCellClick(e) {
+        if (e.target.textContent === "" && gameCompleted === false) {
+            e.target.textContent = currentPlayerTurn === 1 ? "O" : "X"
+            roundNumber++
+            gameScreenUI.roundDisplayText.textContent = `Round: ${roundNumber}`
+            if (checkForWinner() === true) {
+                endSequence(currentPlayerTurn, true, playerOne, playerTwo)
+                gameCompleted = true
+                return
+            }
+            currentPlayerTurn = currentPlayerTurn === 1 ? 2 : 1
+            playerDisplayFocus(currentPlayerTurn)
+            if (roundNumber === 9) {
+                endSequence(currentPlayerTurn, false, playerOne, playerTwo)
+                gameCompleted = true
+                return
+            }
+        }
+        else if (gameCompleted === true) {
+            alert("Game is finished.")
+        }
+    
+        else {
+            alert("Cell already filled.")
+        }
+    }
 }
 
+function playerDisplayFocus(currentPlayerTurn) {
+    let gameScreenUI = gameScreenElements()
+    if (currentPlayerTurn === 1) {
+        gameScreenUI.playerOneScore.classList.add("focusPlayer")
+        gameScreenUI.playerTwoScore.classList.remove("focusPlayer")
+    }
+    else {
+        gameScreenUI.playerTwoScore.classList.add("focusPlayer")
+        gameScreenUI.playerOneScore.classList.remove("focusPlayer")
+    }
+}
+
+function checkForWinner() {
+    let cell1 = document.querySelector("#cell-1").textContent
+    let cell2 = document.querySelector("#cell-2").textContent
+    let cell3 = document.querySelector("#cell-3").textContent
+    let cell4 = document.querySelector("#cell-4").textContent
+    let cell5 = document.querySelector("#cell-5").textContent
+    let cell6 = document.querySelector("#cell-6").textContent
+    let cell7 = document.querySelector("#cell-7").textContent
+    let cell8 = document.querySelector("#cell-8").textContent
+    let cell9 = document.querySelector("#cell-9").textContent
+
+    if (cell1 != "" && cell1 == cell2 && cell1 == cell3) {
+        return true
+    }
+    if (cell4 != "" && cell4 == cell5 && cell4 == cell6) {
+        return true
+    }
+    if (cell7 != "" && cell7 == cell8 && cell7 == cell9) {
+        return true
+    }
+    if (cell1 != "" && cell1 == cell4 && cell1 == cell7) {
+        return true
+    }
+    if (cell2 != "" && cell2 == cell5 && cell2 == cell8) {
+        return true
+    }
+    if (cell3 != "" && cell3 == cell6 && cell3 == cell9) {
+        return true
+    }
+    if (cell1 != "" && cell1 == cell5 && cell1 == cell9) {
+        return true
+    }
+    if (cell3 != "" && cell3 == cell5 && cell3 == cell7) {
+        return true
+    }
+}
+
+function endSequence(currentPlayerTurn, isWin, playerOne, playerTwo) {
+    let gameScreenUI = gameScreenElements()
+    if (currentPlayerTurn === 1 && isWin === true) {
+        playerOne.win()
+        gameScreenUI.playerOneScore.textContent = `${playerOne.name}: ${playerOne.points}`
+        gameScreenUI.roundDisplayText.textContent = `${playerOne.name} Wins!`
+    }
+    if (currentPlayerTurn === 2 && isWin === true) {
+        playerTwo.win()
+        gameScreenUI.playerTwoScore.textContent = `${playerTwo.name}: ${playerTwo.points}`
+        gameScreenUI.roundDisplayText.textContent = `${playerTwo.name} Wins!`
+    }
+
+    let completedButtons = document.createElement("div")
+    completedButtons.id = "completedButtons"
+    completedButtons.innerHTML =
+    `
+    <div id="newGameButton" class="button" type=button>
+        New Game
+    </div>
+    <div id="returnButton" class="button" type=button>
+        Return
+    </div>
+    `
+    gameScreenUI.gameBoard.appendChild(completedButtons)
+    gameScreenUI.roundDisplay.style.marginBottom = "0px"
+
+    let newGameButton = document.querySelector("#newGameButton")
+    let returnButton = document.querySelector("#returnButton")
+
+    newGameButton.addEventListener("click", () =>{
+        for (const cell in gameScreenUI.cells) {
+            cell.textContent = ""
+        }
+        gameManager(playerOne, playerTwo)
+    })
+    returnButton.addEventListener("click", () => {
+        window.location.reload()
+    })
+}
 
 screenManager()
